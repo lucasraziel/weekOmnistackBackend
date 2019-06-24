960af14d2e6fc54816bb56730292921b5e8edd26
root <root@DESKTOP-77RQ0M5.localdomain>
2019-06-23 21:27:37 -0300

    env

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
-
+require('dotenv').config;
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect(
-	'mongodb+srv://admin:lete0704@cluster0-vmi5z.mongodb.net/test?retryWrites=true&w=majority',
+	`mongodb+srv://${process.env.DB_USER}:${
+		process.env.DB_PASSWORD
+	}@cluster0-vmi5z.mongodb.net/test?retryWrites=true&w=majority`,
 	{
 		useNewUrlParser: true
 	}
